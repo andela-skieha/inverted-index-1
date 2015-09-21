5127a8b6b33f0f96d147713bf8e8b19bec842b20
Eugene Mutai <eugene.mutai@andela.com>
2015-09-21 15:40:26 +0300

    Did a few refactoring, majoring on spacing, good practice encourages 2 spaces

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -1,118 +1,113 @@
 describe("Inverted index curriculum tests", function() {
-   // Instantiate the inverted index class
-   var invertedIndex = new Index(),
-      booksObject = null;
+  // Instantiate the inverted index class
+  var invertedIndex = new Index(),
+    booksObject = null;
 
-   beforeEach(function(done) {
-      // Read the book json file and assert the book has been read.
-      invertedIndex.createIndex('../src/books.json').then(function(json) {
-         // console.log(json);
-         booksObject = json;
-         done();
-      });
-   });
+  beforeEach(function(done) {
+    // Read the book json file and assert the book has been read.
+    invertedIndex.createIndex('../src/books.json').then(function(json) {
+      // console.log(json);
+      booksObject = json;
+      done();
+    });
+  });
 
-   describe("Reads book data", function() {
+  describe("Reads book data", function() {
 
-      it("reads the book data into an object", function(done) {
-         expect(booksObject).not.toBe(null);
-         done();
-      });
+    it("reads the book data into an object", function(done) {
+      expect(booksObject).not.toBe(null);
+      done();
+    });
 
-      it("the JSON array created is not empty", function(done) {
-         expect(booksObject.length).toBe(2);
-         done();
-      });
+    it("the JSON array created is not empty", function(done) {
+      expect(booksObject.length).toBe(2);
+      done();
+    });
 
-   });
+  });
 
-   describe("Populates index", function() {
+  describe("Populates index", function() {
 
-      it("ensure index is created", function(done) {
-         var index = invertedIndex.getIndex();
-         expect(typeof index).toBe(typeof new Object());
-         expect(index.alice).toBeDefined();
-         done();
-      });
+    it("ensure index is created", function(done) {
+      var index = invertedIndex.getIndex();
+      expect(typeof index).toBe(typeof new Object());
+      expect(index.alice).toBeDefined();
+      done();
+    });
 
-      it("ensure index is correct", function(done) {
-         var index = invertedIndex.getIndex(),
-            indexProperties = Object.keys(index);
-         // console.log(indexProperties);
+    it("ensure index is correct", function(done) {
+      var index = invertedIndex.getIndex(),
+        indexProperties = Object.keys(index);
+      // console.log(indexProperties);
 
-         expect(indexProperties).toContain("alice");
-         expect(indexProperties).not.toContain("in");
-         expect(indexProperties).toContain("wonderland");
-         expect(indexProperties).toContain("lord");
-         expect(indexProperties).not.toContain("of");
-         expect(indexProperties).not.toContain("the");
-         expect(indexProperties).toContain("rings");
+      expect(indexProperties).toContain("alice");
+      expect(indexProperties).not.toContain("in");
+      expect(indexProperties).toContain("wonderland");
+      expect(indexProperties).toContain("lord");
+      expect(indexProperties).not.toContain("of");
+      expect(indexProperties).not.toContain("the");
+      expect(indexProperties).toContain("rings");
+      done();
+    });
 
-         done();
-      });
+  });
 
-   });
+  describe("Search index", function() {
 
-   describe("Search index", function() {
+    it("returns the correct results when searched", function(done) {
 
-      it("returns the correct results when searched", function(done) {
+      expect(invertedIndex.searchIndex("alison")).toBe(-1);
+      expect(typeof invertedIndex.searchIndex("alice")).toBe(typeof new Array());
+      expect(invertedIndex.searchIndex("alice")).toContain(0);
+      done();
+    });
 
-         expect(invertedIndex.searchIndex("alison")).toBe(-1);
-         expect(typeof invertedIndex.searchIndex("alice")).toBe(typeof new Array());
-         expect(invertedIndex.searchIndex("alice")).toContain(0);
-         done();
+    it("the search function can handle a varied number of search terms as arguments", function(done) {
 
-      });
+      // All results should reflect -1 for words that do not exist in the index
+      expect(invertedIndex.searchIndex("ashley", "madison")).not.toContain(0);
+      expect(invertedIndex.searchIndex("ashley", "madison", "hack")).not.toContain(1);
 
-      it("the search function can handle a varied number of search terms as arguments", function(done) {
+      // If the words exists, the results should either be 1 or 0
+      expect(invertedIndex.searchIndex("alice", "rings")).toContain([1]);
+      expect(invertedIndex.searchIndex("alice", "rings", "dwarf")).toContain([0]);
+    });
 
-         // All results should reflect -1 for words that do not exist in the index
-         expect(invertedIndex.searchIndex("ashley", "madison")).not.toContain(0);
-         expect(invertedIndex.searchIndex("ashley", "madison", "hack")).not.toContain(1);
+    it("searchIndex can handle an array of search terms", function(done) {
 
-         // If the words exists, the results should either be 1 or 0
-         expect(invertedIndex.searchIndex("alice", "rings")).toContain([1]);
-         expect(invertedIndex.searchIndex("alice", "rings", "dwarf")).toContain([0]);
-      });
+      // pass an array
+      expect(invertedIndex.searchIndex(["alice", "rings"])).toContain([1]);
+      expect(invertedIndex.searchIndex(["alice", "rings", "dwarf"])).toContain([0]);
+      done();
+    });
 
-      it("searchIndex can handle an array of search terms", function(done) {
+    it("search does not take too long to execute", function() {
+      // should not take more than 0.02 seconds or 20 nanoseconds
+      var timeTaken = 0,
+        timeLimit = 20,
+        results = [];
 
-         // pass an array
-         expect(invertedIndex.searchIndex(["alice", "rings"])).toContain([1]);
-         expect(invertedIndex.searchIndex(["alice", "rings", "dwarf"])).toContain([0]);
-         done();
+      // start a countup, a very fast one
+      // counts up every 2 nano second
+      var countUp = setInterval(function() {
+        // if the results are defined stop the count up
+        if (results.length !== 0) {
+          clearInterval(countUp);
+        }
+        timeTaken += 2;
+      }, 2);
 
-      });
+      // Let's even delay it by 5 nanoseconds
+      setTimeout(function() {
+        // Make the list of arguments long to add more computation
+        var args = ["alice", "rings", "lord", "wonderland", "enters", "imagination", "hole", "rabbit", "world", "elf", "dwarf", "hobbit", "wizard", "destroy", "ring", "seek", "alliance"];
 
-      it("search does not take too long to execute", function() {
-         // should not take more than 0.02 seconds or 20 nanoseconds
-         var timeTaken = 0,
-            timeLimit = 20,
-            results = [];
+        // run the search
+        results = invertedIndex.searchIndex(args);
+        // console.log(timeTaken);
+      }, 5);
 
-         // start a countup, a very fast one
-         // counts up every 2 nano second
-         var countUp = setInterval(function() {
-            // if the results are defined stop the count up
-            if (results.length !== 0) {
-               clearInterval(countUp);
-            }
-            timeTaken += 2;
-         }, 2);
-
-         // Let's even delay it by 5 nanoseconds
-         setTimeout(function() {
-            // Make the list of arguments long to add more computation
-            var args = ["alice", "rings", "lord", "wonderland", "enters", "imagination", "hole", "rabbit", "world", "elf", "dwarf", "hobbit", "wizard", "destroy", "ring", "seek", "alliance"];
-
-            // run the search
-            results = invertedIndex.searchIndex(args);
-            // console.log(timeTaken);
-         }, 5);
-
-         expect(timeTaken).toBeLessThan(timeLimit);
-      });
-
-   })
-
+      expect(timeTaken).toBeLessThan(timeLimit);
+    });
+  })
 })
--- src/invertedIndex.js
+++ src/invertedIndex.js
@@ -2,110 +2,110 @@
 // var InvertedIndex = function() {}
 
 var Index = function() {
-   this.notAcceptedCharacters = ["a", "an", "and", "or", "the", "for", "of", "into", "to", "in"];
-   this.dictionary = {};
+  this.notAcceptedCharacters = ["a", "an", "and", "or", "the", "for", "of", "into", "to", "in"];
+  this.dictionary = {};
 };
 
 Index.prototype = {
-   createIndex: function(filepath) {
-      var index = this;
-      // Read the JSON file get the books and create an index
-      return index.readJSONfile(filepath, function(jsonContent) {
-         index.populateIndex(jsonContent);
+  createIndex: function(filepath) {
+    var index = this;
+    // Read the JSON file get the books and create an index
+    return index.readJSONfile(filepath, function(jsonContent) {
+      index.populateIndex(jsonContent);
+    });
+  },
+  populateIndex: function(bookArray) {
+    var that = this,
+      bookObjectPosition = 0;
+
+    // Loop through the book extracts
+    Array.prototype.forEach.call(bookArray, function(book) {
+      // concatenate the book title and text, to get all the words for a proper search
+      var allTextualContent = (book.title + " " + book.text).replace(/(\.|\,|\:|\;)/g, "");
+
+      // split the concatenated string to get all the words
+      var allTheWords = allTextualContent.split(/\s/);
+
+      // Loop through them adding them to the dictonary object if they are already existing
+      allTheWords.forEach(function(word) {
+        var word = word.toLowerCase(),
+          dict = that.dictionary;
+
+        // check to see that it does no exist
+        if (!dict.hasOwnProperty(word) && that.notAcceptedCharacters.indexOf(word) == -1) {
+          // then add it
+          dict[word] = [bookObjectPosition];
+        }
       });
-   },
-   getIndex: function() {
-      return this.dictionary;
-   },
-   searchIndex: function(search_terms) {
-      var results = [];
 
-      if (arguments.length > 1) {
-         // eg. searchIndex("home", "hole", "king", "hobbit"...)
-         search_terms = arguments;
-      } else {
-         // If an array was not passed then a single term was passed
-         if (Object.prototype.toString.call(search_terms) !== '[object Array]') {
-            // wrap it up to a single value in an array
-            search_terms = [search_terms];
-         }
+      bookObjectPosition++;
+    })
+  },
+  searchIndex: function(search_terms) {
+    var results = [];
+
+    if (arguments.length > 1) {
+      // eg. searchIndex("home", "hole", "king", "hobbit"...)
+      search_terms = arguments;
+    } else {
+      // If an array was not passed then a single term was passed
+      if (Object.prototype.toString.call(search_terms) !== '[object Array]') {
+        // wrap it up to a single value in an array
+        search_terms = [search_terms];
       }
-      // console.log("Arg", search_terms)
+    }
+    // console.log("Arg", search_terms)
 
-      // Now check for the existence of every word in the array
-      for (var x = 0, searchSize = search_terms.length; x < searchSize; x++) {
-         var index = this.findIndex(search_terms[x]);
-         results.push(index);
+    // Now check for the existence of every word in the array
+    for (var x = 0, searchSize = search_terms.length; x < searchSize; x++) {
+      var index = this.findIndex(search_terms[x]);
+      results.push(index);
+    }
+
+    if (arguments.length == 1 && Array.isArray(arguments[0]) == false) {
+      return results[0];
+    }
+    return results;
+  },
+  findIndex: function(word) {
+    var dict = this.getIndex(),
+      word = word.toLowerCase();
+
+    if (dict[word]) {
+      return dict[word];
+    }
+    return -1;
+  },
+  getIndex: function() {
+    return this.dictionary;
+  },
+  readJSONfile: function(filepath, callback) {
+    var that = this;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200 || window.location.href.indexOf("http") == -1) {
+          that.jsonContent = JSON.parse(xmlhttp.responseText);
+          // console.log("The JSON Content: ", that.jsonContent);
+          callback(that.jsonContent);
+        } else {
+          console.error("An error has occured making the request");
+          return false;
+        }
       }
+    }
 
-      if (arguments.length == 1 && Array.isArray(arguments[0]) == false) {
-         return results[0];
-      }
-      return results;
-   },
-   findIndex: function(word) {
-      var dict = this.getIndex(),
-         word = word.toLowerCase();
-
-      if (dict[word]) {
-         return dict[word];
-      }
-      return -1;
-   },
-   populateIndex: function(bookArray) {
-      var that = this,
-         bookObjectPosition = 0;
-
-      // Loop through the book extracts
-      Array.prototype.forEach.call(bookArray, function(book) {
-         // concatenate the book title and text, to get all the words for a proper search
-         var allTextualContent = (book.title + " " + book.text).replace(/(\.|\,|\:|\;)/g, "");
-
-         // split the concatenated string to get all the words
-         var allTheWords = allTextualContent.split(/\s/);
-
-         // Loop through them adding them to the dictonary object if they are already existing
-         allTheWords.forEach(function(word) {
-            var word = word.toLowerCase(),
-               dict = that.dictionary;
-
-            // check to see that it does no exist
-            if (!dict.hasOwnProperty(word) && that.notAcceptedCharacters.indexOf(word) == -1) {
-               // then add it
-               dict[word] = [bookObjectPosition];
-            }
-         });
-
-         bookObjectPosition++;
-      })
-   },
-   readJSONfile: function(filepath, callback) {
-      var that = this;
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-         if (xmlhttp.readyState == 4) {
-            if (xmlhttp.status == 200 || window.location.href.indexOf("http") == -1) {
-               that.jsonContent = JSON.parse(xmlhttp.responseText);
-               // console.log("The JSON Content: ", that.jsonContent);
-               callback(that.jsonContent);
-            } else {
-               console.error("An error has occured making the request");
-               return false;
-            }
-         }
-      }
-
-      xmlhttp.open("GET", filepath, true);
-      xmlhttp.setRequestHeader("Content-Type", "application/json");
-      xmlhttp.send(null);
-      return this
-   },
-   then: function(callback) {
-      var that = this;
-      if (typeof callback == "function") {
-         setTimeout(function() {
-            callback(that.jsonContent);
-         }, 0);
-      }
-   }
+    xmlhttp.open("GET", filepath, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(null);
+    return this
+  },
+  then: function(callback) {
+    var that = this;
+    if (typeof callback == "function") {
+      setTimeout(function() {
+        callback(that.jsonContent);
+      }, 0);
+    }
+  }
 }
